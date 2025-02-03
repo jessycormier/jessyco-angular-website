eb5a496e7b9aba7c8bee8b1eb45c17e3eede845f
Jessy Cormier <jessy@jessy.co>
2025-02-03 13:58:15 -0400

    fix bundle thing, move to single style sheet approach

--- src/app/components/link/link.component.ts
+++ src/app/components/link/link.component.ts
@@ -1,12 +1,10 @@
+import { Component, Input } from '@angular/core';
 import { RouterLink, UrlTree } from '@angular/router';
-import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-link',
   imports: [RouterLink],
   templateUrl: './link.component.html',
-  styleUrls: ['./link.css'],
-  encapsulation: ViewEncapsulation.Emulated,
 })
 export class LinkComponent {
   @Input() link!: string | any[] | UrlTree | null | undefined;
