c19102f96230fb423aa7151205df0cc281ade693
Jessy Cormier <jessy@jessy.co>
2025-02-18 13:56:15 -0400

    update site navigation ui/ux

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,17 +1,18 @@
 import { Component, effect, HostListener } from '@angular/core';
 import { LayoutService } from '@jc/services/layout.service';
 import { LinkComponent } from '../link/link.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [LinkComponent],
+  imports: [LinkComponent, CommonModule],
   templateUrl: './menu.component.html',
 })
 export class MenuComponent {
-  isShown = '';
+  isShown = false;
   constructor(private layout: LayoutService) {
     effect(() => {
-      this.isShown = this.layout.menu() ? '' : 'translate-x-full';
+      this.isShown = this.layout.menu();
     });
   }
 
